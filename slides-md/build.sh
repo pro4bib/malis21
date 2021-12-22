b856ba1c43c3c192ec7d3fe5ecf48c6a584f1f1f
Adrian Pohl <pohl@hbz-nrw.de>
2021-12-22 12:03:53 +0100

    Update build.sh
    ...with parameter --include-untracked as I do not want to
    have the /slides folder with the static slides in the
    main branch.

--- slides-md/build.sh
+++ slides-md/build.sh
@@ -2,8 +2,9 @@
 
 reveal-md --static ~/git/malis21/slides ~/git/malis21/slides-md --theme blood
 cd ~/git/malis21
-git stash
+git stash --include-untracked
 git checkout pages
 git stash pop
 git add .
 git ci -m "Publish build from ${CURRENTEPOCTIME}"
+git checkout main
