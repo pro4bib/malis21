e15d0a3b2f09cd9d1271b090ed7d0dd5684409ff
Adrian Pohl <pohl@hbz-nrw.de>
2021-12-22 12:30:40 +0100

    Update build.sh to inlcude date in commit

--- slides-md/build.sh
+++ slides-md/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+NOW=`date '+%F_%H:%M:%S'`
+
 reveal-md --static ~/git/malis21/slides ~/git/malis21/slides-md --theme blood
 cd ~/git/malis21
 git stash --include-untracked
 git checkout pages
 git stash pop
 git add .
-git ci -m "Publish build from ${CURRENTEPOCTIME}"
+git ci -m "Publish build from $NOW"
+git push origin pages
 git checkout main
